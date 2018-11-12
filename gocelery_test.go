5698763c4514dc36e2600e31bd8e9de36801844c
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2018-11-12 13:00:29 +0530

    Disabling amqp in tests

--- gocelery_test.go
+++ gocelery_test.go
@@ -82,25 +82,25 @@ func (st *stateFulTask) RunTask() (interface{}, error) {
 func getAMQPClient() (*CeleryClient, error) {
 	amqpBroker := NewAMQPCeleryBroker("amqp://")
 	amqpBackend := NewAMQPCeleryBackend("amqp://")
-	return NewCeleryClient(amqpBroker, amqpBackend, 4, 100)
+	return NewCeleryClient(amqpBroker, amqpBackend, 4, 1)
 }
 
 func getRedisClient() (*CeleryClient, error) {
 	redisBroker := NewRedisCeleryBroker("redis://localhost:6379")
 	redisBackend := NewRedisCeleryBackend("redis://localhost:6379")
-	return NewCeleryClient(redisBroker, redisBackend, 1, 100)
+	return NewCeleryClient(redisBroker, redisBackend, 1, 1)
 }
 
 func getInMemoryClient(numWorkers int) (*CeleryClient, error) {
 	inMemoryBroker := NewInMemoryBroker()
 	inMemoryBackend := NewInMemoryBackend()
-	return NewCeleryClient(inMemoryBroker, inMemoryBackend, numWorkers, 500)
+	return NewCeleryClient(inMemoryBroker, inMemoryBackend, numWorkers, 1)
 }
 
 func getLevelDBClient(numWorkers int, db *leveldb.DB, queue string) (*CeleryClient, error) {
 	broker := NewLevelDBBroker(db, queue)
 	backend := NewLevelDBBackend(db)
-	return NewCeleryClient(broker, backend, numWorkers, 100)
+	return NewCeleryClient(broker, backend, numWorkers, 1)
 }
 
 func getClients(db *leveldb.DB, queue string) ([]*CeleryClient, error) {
