64193b8e4792624c90ec382279c8b5e38af3123e
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2018-11-12 10:05:35 +0530

    Fix test concurrency issue

--- gocelery_test.go
+++ gocelery_test.go
@@ -82,19 +82,19 @@ func (st *stateFulTask) RunTask() (interface{}, error) {
 func getAMQPClient() (*CeleryClient, error) {
 	amqpBroker := NewAMQPCeleryBroker("amqp://")
 	amqpBackend := NewAMQPCeleryBackend("amqp://")
-	return NewCeleryClient(amqpBroker, amqpBackend, 4, 0)
+	return NewCeleryClient(amqpBroker, amqpBackend, 4, 1)
 }
 
 func getRedisClient() (*CeleryClient, error) {
 	redisBroker := NewRedisCeleryBroker("redis://localhost:6379")
 	redisBackend := NewRedisCeleryBackend("redis://localhost:6379")
-	return NewCeleryClient(redisBroker, redisBackend, 1, 0)
+	return NewCeleryClient(redisBroker, redisBackend, 1, 1)
 }
 
 func getInMemoryClient(numWorkers int) (*CeleryClient, error) {
 	inMemoryBroker := NewInMemoryBroker()
 	inMemoryBackend := NewInMemoryBackend()
-	return NewCeleryClient(inMemoryBroker, inMemoryBackend, numWorkers, 0)
+	return NewCeleryClient(inMemoryBroker, inMemoryBackend, numWorkers, 1)
 }
 
 func getLevelDBClient(numWorkers int, db *leveldb.DB, queue string) (*CeleryClient, error) {
--- worker.go
+++ worker.go
@@ -14,7 +14,7 @@ type CeleryWorker struct {
 	broker          CeleryBroker
 	backend         CeleryBackend
 	numWorkers      int
-	waitTimeMS      int
+	waitTimeMicroS  int
 	registeredTasks map[string]interface{}
 	taskLock        sync.RWMutex
 	stopChannel     chan struct{}
@@ -22,13 +22,13 @@ type CeleryWorker struct {
 }
 
 // NewCeleryWorker returns new celery worker
-func NewCeleryWorker(broker CeleryBroker, backend CeleryBackend, numWorkers int, waitTimeMS int) *CeleryWorker {
+func NewCeleryWorker(broker CeleryBroker, backend CeleryBackend, numWorkers int, waitTimeMicroS int) *CeleryWorker {
 	return &CeleryWorker{
 		broker:          broker,
 		backend:         backend,
 		numWorkers:      numWorkers,
 		registeredTasks: make(map[string]interface{}),
-		waitTimeMS:      waitTimeMS,
+		waitTimeMicroS:  waitTimeMicroS,
 	}
 }
 
@@ -37,7 +37,7 @@ func (w *CeleryWorker) StartWorker() {
 
 	w.stopChannel = make(chan struct{})
 	w.workWG.Add(w.numWorkers)
-	ticker := time.NewTicker(time.Millisecond * time.Duration(w.waitTimeMS))
+	ticker := time.NewTicker(time.Microsecond * time.Duration(w.waitTimeMicroS))
 
 	for i := 0; i < w.numWorkers; i++ {
 		go func(workerID int) {
